9d727771e9111d0d31416b395172ae2ac382da60
Brooke G <brooke.a.guarienti@gmail.com>
2020-02-21 14:56:15 -0600

    updated html with ids for game components. updated

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+var wins = 0
+var currentWord = 0
+var numGuess = 0
+var letter = 0
+
+var animals = [Lion, Tiger, Bear, Chicken, Pig]
+
+if (userGuess)
+
+
+
+var winsText = document.getElementById("wins-text");
+var currentWordText = document.getElementById("current-word");
+var numGuess = document.getElementById("num-guess");
+var letter = document.getElementById("letter");
+
+
+document.onkeyup = function (event){
+
+}
