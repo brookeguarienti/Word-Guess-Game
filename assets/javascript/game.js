05b478905e214b23ebebb8de76244edd66b64232
Brooke G <brooke.a.guarienti@gmail.com>
2020-02-24 15:35:46 -0600

    added global variables and functions to allow onkeyup, restart, and fixed function to render Animals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,34 @@
 // Start the game with a score of 0
-var wins = 0
+var wins = 0;
 
 // Array of animal words
-var animals = ["Lion", "Tiger", "Bear", "Chicken", "Pig"]
+var animals = ["Lion", "Tiger", "Bear", "Chicken", "Pig"];
 
 // Variable to hold the index of current question
-var animalsIndex = 0
+var animalsIndex = 0;
+
+// Answer Array
+var answerArray = [];
+
+// Number of guesses availble
+numGuessLeft = 10;
+
+// display letters guessed 
+var incorrectGuesses = [];
+
+// 
+var animalsGuess = "";
+
+//
+var letter = "";
+
+//
+var restart = function() {
+  numGuessLeft = 0;
+  incorrectGuesses = [];
+  numGuess = numGuessLeft;
+  letter = "";
+}
 
 // Create variables that hold references to the places in the HTML where we want to display things.
 var winsText = document.getElementById("wins-text");
@@ -18,13 +41,23 @@ var letter = document.getElementById("letter");
 // FUNCTIONS
 // ==================================================================================================
 
-// Function to render questions.
+// Function to render word.
 function renderAnimals() {
+
+  //Choose a random word from animals
+  animalsGuess = animals[Math.floor(Math.random() * animals.length)];
+
     // If there are still more animals, render the next one.
-    if (animalsIndex <= (animals.length - 1)) {
-      document.querySelector("#current-word").innerHTML = animals[animalIndex];
+    // if (animalsIndex <= (animals.length - 1)) {
+      
+
+      for (var i = 0; i < animalsGuess.length; i++) {
+        answerArray[i] = "_";
     }
-  }
+
+    currentWordText.innerHTML = answerArray.join(' ');
+    }
+  // }
   // Function that updates the wins
   function updateWins() {
     document.querySelector("#wins").innerHTML = wins;
@@ -50,13 +83,19 @@ document.onkeyup = function (event) {
     // Determine which key was pressed, make it lowercase, and set it to the userInput variable.
     var userGuess = event.key.toLowerCase();
 
-    //Choose a random word from animals
-    var animals = animals[Math.floor(math.random() * animals.length)];
+    // If userGuess does not match a letter in the word, then...
+    if (animalsGuess.indexOf(userGuess) < 0) {
 
-    //
-    for (var i = 0; i < animals.length; i++) {
-        answerArray[i] = "_";
+      // numGuess will decrease 
+      numGuessLeft--;
+      if (numGuessLeft === 0) {
+        restart()
+      }
+      // display num-guess guess
+      document.querySelector("#num-guess").innerHTML = numGuessLeft;
+
+      incorrectGuesses.push(userGuess)
+      document.querySelector("#letter").innerHTML = incorrectGuesses.join(',');
+
     }
-
-    if ((userGuess))
 }
